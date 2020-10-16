10f14959e82edccf4c7fab9b7a78b2eeb309d632
wisnupratama24 <wisnuputrapratama24@gmail.com>
2020-10-16 18:32:06 +0700

    update

--- backend/views/akt-pembelian-detail/update_langsung.php
+++ backend/views/akt-pembelian-detail/update_langsung.php
@@ -2,8 +2,6 @@
 
 use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-/* @var $model backend\models\AktPembelianDetail */
 
 $this->title = 'Ubah Data Pembelian Detail';
 ?>
@@ -12,11 +10,11 @@ $this->title = 'Ubah Data Pembelian Detail';
     <h1><?= Html::encode($this->title) ?></h1>
     <ul class="breadcrumb">
         <li><a href="/">Home</a></li>
-        <li><?= Html::a('Data Order Pembelian', ['akt-pembelian/index']) ?></li>
-        <li><?= Html::a('Detail Data Order Pembelian : ' . $akt_pembelian->no_order_pembelian, ['akt-pembelian/view', 'id' => $model->id_pembelian]) ?></li>
+        <li><?= Html::a('Data Pembelian', ['akt-pembelian-pembelian/index']) ?></li>
+        <li><?= Html::a('Detail Data Pembelian : ' . $akt_pembelian->no_pembelian, ['akt-pembelian-pembelian/view', 'id' => $model->id_pembelian]) ?></li>
         <li class="active"><?= $this->title ?></li>
     </ul>
-    <?= $this->render('_form', [
+    <?= $this->render('_form_update_pembelian_langsung', [
         'model' => $model,
         'akt_pembelian' => $akt_pembelian,
         'data_item_stok' => $data_item_stok,
